9d41a1850162c68ad0cc9d71e53642aaf90d7d74
ArdaArslan35 <481257@student.saxion.nl>
2024-09-08 21:03:31 +0200

    !!

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Inloggen Arda Arslan 481257</label>
+<label class="link" for="signinCheckbox">Inloggen hier!!</label>
 """
 
     return f"""<!DOCTYPE html>
